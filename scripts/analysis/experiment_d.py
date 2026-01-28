d75e8d852c1e5d821187b0a378efb6c9c4b5a3a6
KrainskiL <lukasz.krainski123@gmail.com>
2026-01-28 14:44:37 +0100

    Vizualization scripts

--- scripts/analysis/experiment_d.py
+++ scripts/analysis/experiment_d.py
@@ -4,15 +4,57 @@ import pandas as pd
 
 # Data preparation
 df = pd.read_csv(
-    "./experiments/experiment_d/bigreal/Freebase/evaluate/divergence_scores.csv",
+    "./data/evaluate/experiment_d/bigreal/Freebase/divergence_scores.csv",
     header=0,
 )
 df = df.iloc[: df.shape[0] - 2, :]
 df.drop(columns=["mean_divergence"], inplace=True)
-df["d"] = df["index"].str[-3:-2]
-df = df.melt(id_vars=["index", "d"], var_name="Divergence Metric", value_name="value")
-ax = sns.lineplot(data=df, x="d", y="value", hue="Divergence Metric", marker="o")
-# ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+df["d"] = df["graph"].str[-3:-2]
+df["estimation_method"] = df.graph.str.contains("fancy").map(
+    {True: "fancy_r", False: "rudimentary"}
+)
+df = df.melt(
+    id_vars=["graph", "d", "estimation_method"],
+    var_name="Divergence Metric",
+    value_name="value",
+)
+rename_map = {
+    "R_edges_correlation": "R",
+    "tau_degrees_correlation": "τ",
+    "r_communities_correlation": "r",
+    "gamma_degree_distribution": "γ",
+    "beta_community_sizes_distribution": "β",
+    "xi_intercommunity_noise": "ξ",
+}
+df["Divergence Metric"] = df["Divergence Metric"].replace(rename_map)
+
+# Plotting fancy
+ax = sns.barplot(
+    data=df[df.estimation_method == "fancy_r"],
+    x="Divergence Metric",
+    y="value",
+    hue="d",
+)
+ax.set_ylim(0, 1)
+plt.title("Advanced configuration retrieval")
 plt.ylabel("Divergence Score")
-plt.xlabel("d")
-plt.show()
+plt.xlabel("Divergence Metric")
+plt.savefig("./data/evaluate/experiment_d/bigreal/Freebase/divergenceby_d_fancy_r.pdf")
+plt.clf()
+
+# Plotting rudimentary
+
+ax = sns.barplot(
+    data=df[df.estimation_method == "rudimentary"],
+    x="Divergence Metric",
+    y="value",
+    hue="d",
+)
+ax.set_ylim(0, 1)
+plt.title("Simple configuration retrieval")
+plt.ylabel("Divergence Score")
+plt.xlabel("Divergence Metric")
+plt.savefig(
+    "./data/evaluate/experiment_d/bigreal/Freebase/divergence_by_d_rudimentary.pdf"
+)
+plt.clf()
